1d7024754b6cc81796132b8d05139b46801edeac
Pathik Shah <pathik.shah@ticketmaster.com>
2016-04-01 20:10:16 -0700

    Added mock file to test lambda function locally. Added basic API call to find events.

--- intentHandlers/findEvents.js
+++ intentHandlers/findEvents.js
@@ -1,9 +1,44 @@
+var _ = require('lodash');
+var http = require('http');
+
+const rootUrl = 'http://app.ticketmaster.com/';
+const apiKey = 'kQ988HL4LyHGGJiY89BHZNPNDlXfPKYy';
+
 module.exports = {
 	init: function(request,response) {
 		var SearchTypeSlot = request.slot('SearchType');
 
 		if(SearchTypeSlot) {
-			response.say("Looking for events " + SearchTypeSlot);
+			var url = rootUrl + 'discovery/v1/events.json?apikey=' + apiKey;
+
+			http.get(url, function(res) {
+				var data = '';
+
+				res.on('data', function (chunk){
+			        data += chunk;
+			    });
+
+			    res.on('end',function(){
+			        var obj = JSON.parse(data);
+			        console.log(obj);
+			        var sayResp = _.chain(obj._embedded.events)
+									.map(function(event) {
+										return event.name + ' at ' + event._embedded.venue[0].name;
+									})
+									.value()
+									.toString();
+
+					response.say(sayResp);
+			        response.send();
+			    });
+		    }).on('error', function(e){
+		    	response.say('Sorry, I am unable to pull events currently.');
+		        response.send();
+			    console.log("Got an error: ", e);
+			});
+
+			return false;
+
 		} else {
 			response.say("I'm not sure what you are looking for. You can ask me to find events nearby or find events this weekend");
 		}
---
+++ test/findEventsIntentMock.js
@@ -0,0 +1,27 @@
+module.exports = {
+  session: {
+    sessionId: "SessionId.98603549-ba07-4e31-9648-29bdf68844c2",
+    application: {
+      applicationId: "amzn1.echo-sdk-ams.app.a552d2b7-d520-4f5f-ad06-ed98d3524c45"
+    },
+    user: {
+      userId: "amzn1.ask.account.AFP3ZWPOS2BGJR7OWJZ3DHPKMOMNWY4AY66FUR7ILBWANIHQN73QHBXY6BE6YL2BO6Y3LJGYBDRWMP5IFYIUU5GYFDCP3DMJ4RLAQ7F5EBAOL2DOV7OWCM53ZJJUP6I4VN3ZF5ZC4PEJQRIXB7MJCMCEXGKVSMPWJXDYHSMQBFDIYQ2HABVC3Q43FCZXFK2AUT2X5GZXJUNH64I"
+    },
+    new: true
+  },
+  request: {
+    type: "IntentRequest",
+    requestId: "EdwRequestId.97c681f0-73d9-494b-9935-cbedd8d37fac",
+    timestamp: "2016-04-02T01:07:29Z",
+    intent: {
+      name: "FindEventsIntent",
+      slots: {
+        SearchType: {
+          name: "SearchType",
+          value: "this weekend"
+        }
+      }
+    }
+  },
+  version: "1.0"
+};
